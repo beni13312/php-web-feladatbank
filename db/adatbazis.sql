01defb64ae46454258fb161598365988e20ee0fb
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-02-21 21:30:16 +0100

    Merge remote-tracking branch 'origin/main'

--- db/adatbazis.sql
+++ db/adatbazis.sql
@@ -10,20 +10,20 @@ create table if not exists admin_felhasznalok( -- admin felhasználók
     ido timestamp default current_timestamp
 );
 
-create table if not exists kategoria( -- kategória
+create table if not exists kategoria( -- kategória pl.: hálózat
     id int primary key auto_increment,
     kategoria varchar(50) not null unique,
     ido timestamp default current_timestamp
 );
 
-create table if not exists szint( -- nehézségi szint
+create table if not exists szint( -- nehézségi szint pl.: alapszint
     id int primary key auto_increment,
     szint varchar(50) not null unique,
     ido timestamp default current_timestamp
 );
 
 
-create table if not exists feladat_csoport(
+create table if not exists feladat_csoport( -- belső csoportosítás pl.: Python
     id int primary key auto_increment,
     kat_id int not null,
     csoport varchar(50) not null unique ,
@@ -31,7 +31,7 @@ create table if not exists feladat_csoport(
     foreign key (kat_id) references kategoria(id) on delete cascade
 );
 
-create table if not exists feladatok(
+create table if not exists feladatok( -- feladatok
     id int primary key auto_increment,
     kat_id int not null,
     szint_id int not null,
