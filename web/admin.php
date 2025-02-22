c598da31d1ff78ce774b1e89d2ce5fc11ea1d54b
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-02-22 16:50:39 +0100

    Merge remote-tracking branch 'origin/main'

--- web/admin.php
+++ web/admin.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 
-if(isset($_SESSION['authorized'][0]) && isset($_SESSION['authorized'][1]) && isset($_GET['dashboard'])){
+if(isset($_SESSION['authorized'][0]) && isset($_SESSION['authorized'][1])){
+    if(!isset($_GET['dashboard'])){
+        header('Location: admin.php?dashboard');
+    }
     include("admin_dash.php");
 }else{
     include("admin_login.php");
