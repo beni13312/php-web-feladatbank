f815d4bfff907e6d33783c07e1554418c45bce01
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-02-23 16:02:47 +0100

    Merge remote-tracking branch 'origin/main'

--- web/conn.php
+++ web/conn.php
@@ -5,11 +5,11 @@ $host = "localhost";
 $pass = "";
 $db = "feladatbank";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
 
-if (!$conn) {
-    // echo "<script>console.log('Hiba lépett fel az adatbázis kapcsolódása közben: ".mysqli_connect_error()."');</script>";
-    die("Hiba lépett fel az adatbázis kapcsolódása közben: ".mysqli_connect_error());
-}else{
-    // echo "ok ";
+
+try {
+    $conn = mysqli_connect($host, $user, $pass, $db);
+}catch (mysqli_sql_exception $e){
+    die("Hiba lépett fel az adatbázis kapcsolódása közben: ".$e->getMessage());
 }
+
