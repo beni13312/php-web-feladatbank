037ee03e2843351176ebf6e8f01da34f04535856
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-02-27 20:37:13 +0100

    Merge remote-tracking branch 'origin/main'

--- db/kategoria.sql
+++
@@ -1,8 +0,0 @@
-use feladatbank;
-
-insert into kategoria (kategoria) value ('HTML');
-insert into kategoria (kategoria) value ('CSS');
-insert into kategoria (kategoria) value ('JavaScript');
-insert into kategoria (kategoria) value ('SQL');
-insert into kategoria (kategoria) value ('PHP');
-insert into kategoria (kategoria) value ('Python');
