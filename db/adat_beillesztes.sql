dd8f3287b0eeb277b0120011deea9a4c2401969c
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-03-03 20:14:15 +0100

    Merge remote-tracking branch 'origin/main'

--- db/adat_beillesztes.sql
+++ db/adat_beillesztes.sql
@@ -23,12 +23,12 @@ set @valasz1 = '//';
 set @valasz2 = '#';
 set @valasz3 = '--';
 set @kerdes = 'Mi a jele az egysoros kommentnek?';
-set @megoldas = '#';
+set @megoldas1 = '#';
 
 
 insert into valaszok (valasz) values (@valasz1),(@valasz2),(@valasz3); -- választási lehetőségek
 
-insert into megoldasok (megoldas) value (@megoldas); -- megoldás/megoldások
+insert into megoldasok (megoldas) values (@megoldas1); -- megoldás/megoldások
 
 insert into feladat (kat_id, kerdes) values (
                 (select kategoria.id from kategoria where kategoria.kategoria = @kategoria),@kerdes);
@@ -46,5 +46,48 @@ insert into feladat_valasz (feladat_id, valasz_id) VALUES
                         );
 insert into feladat_megoldas (feladat_id, megoldas_id) VALUES (
                         (select feladat.id from feladat where kerdes=@kerdes),
-                        (select megoldasok.id from megoldasok where megoldas= @megoldas)
+                        (select megoldasok.id from megoldasok where megoldas= @megoldas1)
+                        );
+
+
+
+-- Két megoldás, négy választási lehetőség
+
+set @kategoria = 'PHP';
+set @valasz1 = 'std::cout << "Hello, World!";';
+set @valasz2 = 'printf("Hello, World!");';
+set @valasz3 = 'echo "Hello, World!";';
+set @valasz4 = 'print "Hello, World!";';
+set @kerdes = 'Mi a helyes mód egy szöveg kiíratására PHP-ban?';
+set @megoldas1 = 'echo "Hello, World!";';
+set @megoldas2 = 'print "Hello, World!";';
+
+
+insert into valaszok (valasz) values (@valasz1),(@valasz2),(@valasz3),(@valasz4); -- választási lehetőségek
+
+insert into megoldasok (megoldas) values (@megoldas1),(@megoldas2); -- megoldás/megoldások
+
+insert into feladat (kat_id, kerdes) values (
+                (select kategoria.id from kategoria where kategoria.kategoria = @kategoria),@kerdes);
+
+insert into feladat_valasz (feladat_id, valasz_id) VALUES
+                        (
+                        (select feladat.id from feladat where kerdes = @kerdes),
+                        (select valaszok.id from valaszok where valasz = @valasz1)
+                        ),                        (
+                        (select feladat.id from feladat where kerdes = @kerdes),
+                        (select valaszok.id from valaszok where valasz = @valasz2)
+                        ),                        (
+                        (select feladat.id from feladat where kerdes = @kerdes),
+                        (select valaszok.id from valaszok where valasz = @valasz3)
+                        ),                        (
+                        (select feladat.id from feladat where kerdes = @kerdes),
+                        (select valaszok.id from valaszok where valasz = @valasz4)
+                                                  );
+insert into feladat_megoldas (feladat_id, megoldas_id) VALUES (
+                        (select feladat.id from feladat where kerdes=@kerdes),
+                        (select megoldasok.id from megoldasok where megoldas= @megoldas1)
+                        ),                        (
+                        (select feladat.id from feladat where kerdes=@kerdes),
+                        (select megoldasok.id from megoldasok where megoldas= @megoldas2)
                         );
