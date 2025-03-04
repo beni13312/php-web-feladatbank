6c9d0f1c9b6d50e04d0208523ae6ff8836b166b2
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-03-04 16:48:23 +0100

    Merge remote-tracking branch 'origin/main'

--- web/admin_dash.php
+++ web/admin_dash.php
@@ -23,6 +23,7 @@ if(!isset($_SESSION['authorized'])){
 </head>
 <body>
 <?php require "header/header.php"?>
+<?php include "conn.php"; global $conn; ?>
 <div class="body">
     <div id="admin-flexbox">
         <h1>Admin felület</h1>
@@ -39,11 +40,18 @@ if(!isset($_SESSION['authorized'])){
         <div id="admindash-add">
             <form action="add_exam.php" method="post">
                 <div class="admindash-cat-container">
-                    <select name="category" id="admindash-cat">
-                        <option value="x1">x1</option>
-                        <option value="x2">x2</option>
-                        <option value="x3">x3</option>
-                    </select>
+                    <?php $sql_cat= "SELECT kategoria.id, kategoria.kategoria FROM kategoria";
+                    $query = mysqli_query($conn,$sql_cat);
+
+                    echo '<select name="category" id="admindash-cat">';
+                    echo '<option>Kategória</option>';
+                    if(mysqli_num_rows($query) > 0){
+                        while($row = mysqli_fetch_assoc($query)){
+                            echo '<option value="'.$row['id'].'">'.$row['kategoria'].'</option>';
+                        }
+                    }
+                    echo '</select>';
+                    ?>
                 </div>
                 <div class="admindash-question">
                     <input type="text" name="exam-question" id="admindash-question">
