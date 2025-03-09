0ebc8de14e66883c7781c91a256bc93de91b3991
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-03-09 22:55:34 +0100

    Merge remote-tracking branch 'origin/main'

--- db/adat_beillesztes.sql
+++ db/adat_beillesztes.sql
@@ -56,7 +56,6 @@ insert into feladat_megoldas (feladat_id, megoldas_id) VALUES (
 
 set @kategoria = 'PHP';
 set @valasz1 = 'std::cout << "Hello, World!";';
-set @valasz2 = 'printf("Hello, World!");';
 set @valasz3 = 'echo "Hello, World!";';
 set @valasz4 = 'print "Hello, World!";';
 set @kerdes = 'Mi a helyes mód egy szöveg kiíratására PHP-ban?';
@@ -64,7 +63,7 @@ set @megoldas1 = 'echo "Hello, World!";';
 set @megoldas2 = 'print "Hello, World!";';
 
 
-insert into valaszok (valasz) values (@valasz1),(@valasz2),(@valasz3),(@valasz4); -- választási lehetőségek
+insert into valaszok (valasz) values (@valasz1),(@valasz3),(@valasz4); -- választási lehetőségek
 
 insert into megoldasok (megoldas) values (@megoldas1),(@megoldas2); -- megoldás/megoldások
 
@@ -75,9 +74,6 @@ insert into feladat_valasz (feladat_id, valasz_id) VALUES
                         (
                         (select feladat.id from feladat where kerdes = @kerdes),
                         (select valaszok.id from valaszok where valasz = @valasz1)
-                        ),                        (
-                        (select feladat.id from feladat where kerdes = @kerdes),
-                        (select valaszok.id from valaszok where valasz = @valasz2)
                         ),                        (
                         (select feladat.id from feladat where kerdes = @kerdes),
                         (select valaszok.id from valaszok where valasz = @valasz3)
