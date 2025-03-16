89f467ec394dadd69bf7e0efe7e92d805bb8b162
benjamin balazs23 <benjamin.balazs83@gmail.com>
2025-03-16 23:41:55 +0100

    Merge remote-tracking branch 'origin/main'

--- web/check_ans.php
+++ web/check_ans.php
@@ -7,23 +7,25 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST'){
 
 /* következő kérdés */
 
-$advance = $_POST['exam-advance'];
+$advance = $_POST['quiz-advance'];
 $category = $_SESSION['category'];
 
 if(isset($category) && isset($advance)){
     $_SESSION['feladat'] +=1; // növeljük a sessionben a feladat id-t, a következő feladat érdekében
     $_SESSION['feladat_index'] +=1; // növeljük a sessionben a feladat indexet, hogy megjelenjen a feladat száma az oldalon
     header('Location: quiz_inner.php'); // átirányítjuk a user-t megfelelő oldalra
+    exit;
 }
 
 /* elöző kérdés */
 
-$advance = $_POST['exam-before'];
+$advance = $_POST['quiz-before'];
 
 if(isset($category) && isset($advance)){
     $_SESSION['feladat'] -=1; // csökkentjük a sessionben a feladat id-t, az elöző feladat érdekében
     $_SESSION['feladat_index'] -=1; // csökkentjük a sessionben a feladat indexet, hogy megjelenjen a feladat száma az oldalon
     header('Location: quiz_inner.php'); // átirányítjuk a user-t megfelelő oldalra
+    exit;
 }
 
 /* válasz ellenörzése */
