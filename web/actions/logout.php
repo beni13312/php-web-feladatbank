f7088519baf1c04546499afa11a9b50c6c9b2575
cybercode <77294400+beni13312@users.noreply.github.com>
2025-03-23 15:25:03 +0100

    Delete web directory

--- web/actions/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-if(isset($_SESSION['authorized'])){
-    unset($_SESSION['authorized']);
-    unset($_SESSION['session_id']);
-    header('Location: ../admin.php');
-    exit;
-}
--- web/footer/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="footer">
-    <div id="cright">&#169; </div>
-    <script>document.getElementById("cright").append(new Date().getFullYear())</script>
-</div>
